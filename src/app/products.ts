e54fa24bf5aad73670db0d1c75930421d5e0d047
GeraldDi <59823625+GeraldDi@users.noreply.github.com>
2020-02-26 13:45:51 +0100

    added pics

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,16 +2,19 @@ export const products = [
  {
    name: 'Catfood',
    price: 7.99,
-   description: 'The best cat food'
+   description: 'The best cat food',
+   image:'assets/catfood.jpg'
  },
  {
    name: 'Dogfood',
    price: 6.99,
-   description: 'The best dog food'
+   description: 'The best dog food',
+   image:'assets/dogfood.jpg'
  },
  {
    name: 'Dog leash',
    price: 29.99,
-   description: 'The best dog leash'
+   description: 'The best dog leash',
+   image:'assets/dogleash.jpg'
  }
 ];
