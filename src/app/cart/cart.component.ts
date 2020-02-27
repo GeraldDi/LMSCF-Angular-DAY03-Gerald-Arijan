7f4536b1f67e23937b6ac4c08aa2d48c69329fa5
GeraldDi <59823625+GeraldDi@users.noreply.github.com>
2020-02-27 11:23:23 +0100

    added totalprice

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -10,21 +10,34 @@ import { FormBuilder } from '@angular/forms';
 export class CartComponent implements OnInit {
 	items;
 	checkoutForm;
+  tprice;
 
   constructor(private cartService: CartService, private formBuilder: FormBuilder) {
          this.checkoutForm = this.formBuilder.group({
      name: '',
-     address: ''
+     address: '',
+     emaila: ''
    });
  }
 
   ngOnInit() {
+                   
          this.items = this.cartService.getItems();
+
+         // *ngFor="let tprice of items"
+         
+       let tprice = 0;
+         for (let i=0;i<this.items.length;i++){
+         tprice += this.items[i].price;
+         console.log("tprice:"+tprice)
+         this.tprice =tprice;
+         // console.log("items1:"+this.items[0].price)
+       }
  }
  onSubmit(customerData) {
    // Process checkout data here
-   window.alert('Your order has been submitted successfully!' + customerData);
-     // console.warn('Your order has been submitted', customerData);
+   // window.alert('Your order has been submitted successfully!' + customerData);
+     console.warn('Your order has been submitted', customerData);
 
    this.items = this.cartService.clearCart();
    this.checkoutForm.reset();
